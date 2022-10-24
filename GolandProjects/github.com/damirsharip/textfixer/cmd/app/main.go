5d0ab70141cd215c9ea4aac59c6e8aba01f21b6c
damirsharip <70068263+damirsharip@users.noreply.github.com>
2022-10-24 14:10:06 +0600

    fix

--- GolandProjects/github.com/damirsharip/textfixer/cmd/app/main.go
+++ GolandProjects/github.com/damirsharip/textfixer/cmd/app/main.go
@@ -6,24 +6,14 @@ import (
 	"go.uber.org/zap"
 	handler "textfixer/internal/handler"
 	"textfixer/internal/server"
-	"textfixer/pkg/speller"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	ctx := context.Background()
-	gin.SetMode(gin.ReleaseMode)
 
 	logger, _ := zap.NewProduction()
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
-
-	// initializing docusign service receiving access token for future use
-	err := speller.NewClient()
-	if err != nil {
-		//logger.FatalKV(initCtx, "failed while docusign init: %s", err.Error())
-	}
 
 	//srvc := service.NewService()
 	h := handler.NewHandler(sugar)
